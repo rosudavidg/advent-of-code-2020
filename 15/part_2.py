efdfe8adcb90b39d9a37f84c5d56f93e38070007
rosudavidg <rosudavidg@gmail.com>
2020-12-15 07:51:57 +0200

    add solutions for 15th day

---
+++ 15/part_2.py
@@ -0,0 +1,25 @@
+target_position = 30000000
+
+with open('file.in', 'rt') as fin:
+    in_numbers = list(map(int, fin.readline().split(',')))
+
+numbers = dict()
+
+for i, in_nr in enumerate(in_numbers):
+    numbers[in_nr] = i
+
+i = len(in_numbers)
+last_number = 0
+
+while i != target_position - 1:
+    if last_number in numbers:
+        next_number = i - numbers[last_number]
+    else:
+        next_number = 0
+
+    numbers[last_number] = i
+
+    i += 1
+    last_number = next_number
+
+print(last_number)
